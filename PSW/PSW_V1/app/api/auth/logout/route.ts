d6339ca5d5bf5075b8eedede7f985423cd02b8a7
Lorick-Fouquet <lorick.fqt@gmail.com>
2025-05-06 12:04:47 +0200

    Modif du dossier PSW -> SITE

--- PSW/PSW_V1/app/api/auth/logout/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-import { cookies } from "next/headers";
-
-export async function POST() {
-  (await cookies()).delete("token");
-  
-  return NextResponse.json({ message: "Déconnexion réussie" }, { status: 200 });
-}
