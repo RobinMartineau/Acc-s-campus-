09c594c06f7fe0f60e4086a5090ebaeaa7e08148
sachaleseignoux <leseignouxsacha1@gmail.com>
2025-06-04 21:09:53 +0200

    Code PGS

---
+++ PGS/Code/Utilisateurcs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+
+public class Utilisateur
+{
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+
+    [JsonPropertyName("nom")]
+    public string Nom { get; set; }
+
+    [JsonPropertyName("prenom")]
+    public string Prenom { get; set; }
+
+    [JsonPropertyName("identifiant")]
+    public string Identifiant { get; set; }
+
+    [JsonPropertyName("role")]
+    public string Role { get; set; }
+
+    [JsonPropertyName("mot_de_passe")]
+    public string MotDePasse { get; set; }
+
+    [JsonPropertyName("date_de_naissance")]
+    public DateTime? DateDeNaissance { get; set; }
+
+    [JsonPropertyName("id_classe")]
+    public int? IdClasse { get; set; }
+}
