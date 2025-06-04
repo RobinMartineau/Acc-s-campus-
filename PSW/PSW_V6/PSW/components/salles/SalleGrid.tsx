f53d45c84dd5e2351c2aa775408ce98d8dfe4655
Thomas-Gasche <thomasgasche82@gmail.com>
2025-06-04 23:22:23 +0200

    Add files via upload

---
+++ PSW/PSW_V6/PSW/components/salles/SalleGrid.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { Card, CardContent } from "@/components/ui/card";
+import { Badge } from "@/components/ui/badge";
+import { Lock, Unlock } from "lucide-react";
+import { SallesGridProps } from "@/types";
+
+export function SallesGrid({ salles, onSelectSalle }: SallesGridProps) {
+  return (
+    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4">
+      {salles.map((salle) => (
+        <Card
+          key={salle.id}
+          className={`cursor-pointer hover:shadow-md transition-shadow ${
+            !salle.estUtilisee ? "opacity-70" : ""
+          }`}
+          onClick={() => onSelectSalle(salle)}
+        >
+          <CardContent className="p-4">
+            <div className="flex justify-between items-start mb-7">
+              <h3 className="font-medium">{salle.nom}</h3>
+              {salle.estUtilisee ? (
+                <Lock className="h-4 w-4 text-red-500" />
+              ) : (
+                <Unlock className="h-4 w-4 text-green-500" />
+              )}
+            </div>
+            <div className="mt-2 flex justify-center items-center">
+              <Badge
+                className="text-sm px-8"
+                variant={salle.estUtilisee ? "destructive" : "outline"}
+              >
+                {salle.estUtilisee ? "Occupée" : "Libre"}
+              </Badge>
+            </div>
+          </CardContent>
+        </Card>
+      ))}
+    </div>
+  );
+}
